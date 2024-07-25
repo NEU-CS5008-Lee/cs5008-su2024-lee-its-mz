65a746e31d3f8ec842c04d13c872bf4c69c2a660
mingzhe <ou.mi@northeastern.edu>
2024-07-25 04:43:18 -0400

    first commit

--- assignment11/hw11b.c
+++ assignment11/hw11b.c
@@ -1,5 +1,5 @@
-// name: <your name here>
-// email: <your email here>
+// name: Mingzhe Ou
+// email: ou.mi@northeastern.edu
 
 #include <stdio.h>
 
@@ -8,6 +8,14 @@ int d[20];
 long long int dp(int n) {
 
     // Add your code here
+    if (n <= 0) return 0;
+    if (n == 1) return 1;
+    if (n == 2) return 3;
+
+    if (d[n] != 0) return d[n];
+    
+    d[n] = dp(n-1) + dp(n-2) + dp(n-2);
+    return d[n];
 
 }
 
--- assignment11/hw11c.c
+++ assignment11/hw11c.c
@@ -1,5 +1,5 @@
-// name: <your name here>
-// email: <your email here>
+// name: Mingzhe Ou
+// email: ou.mi@northeastern.edu
 
 #include <stdio.h>
 
@@ -8,7 +8,19 @@ int d[20];
 long long int dp(int n) {
 
     // Add your code here
+    if(n == 0) return 1;
+    if(n == 1) return 0;
+    if(n == 2) return 3;
 
+    long long int result = 3 * dp(n - 2);
+
+    if(d[n] != -1) return d[n];
+
+    for (int k = 4; k <= n; k += 2) {
+        result += 2 * dp(n - k);
+    }
+
+    return result;
 }
 
 int main(void) {
