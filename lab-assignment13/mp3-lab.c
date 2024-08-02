04072f18b6085ba2e26c5c6c90e730847a1b6c81
mingzhe <ou.mi@northeastern.edu>
2024-08-02 13:18:09 -0400

    first commit

--- lab-assignment13/mp3-lab.c
+++ lab-assignment13/mp3-lab.c
@@ -1,19 +1,29 @@
-//enter your email here
-//enter your name here
+//Mingzhe Ou
+//mi.ou@northeastern.edu
 
 #include<stdio.h>
 #include<string.h>
 
 int compression(char arr[], int n, char res[]){
+    int index = 0;
+
+    for(int i = 0; i < n; i++){
+        int count = 1;
+        while(i < n - 1 && arr[i] == arr[i + 1]){
+            count++;
+            i++;
+        }
+
+        res[index++] = arr[i];
+
+        if(count > 1){
+            int count_len = snprintf(res + index, n - index, "%d", count);
+            index += count_len;
+        }
+    }
+    res[index] = '\0';
     
-    
-    
-    
-    //insert your code here
-    
-    
-    
-    
+    return index;
     
 }
  
@@ -24,5 +34,6 @@ int main()
     int r,n=strlen(a);//n is the size of input array
     r=compression(a,n,res);
     printf("length of the compressed string:%d\n",r);
+    printf("compressed string: %s\n", res);
     return 0;
 }
